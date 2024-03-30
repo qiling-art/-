1a2cbf779a4fa94795026102109ec897dd4f34b2
13flix <3382436216@qq.com>
2024-03-30 14:49:51 +0800

    Update datatype.c

--- datatype.c
+++ datatype.c
@@ -2,17 +2,19 @@ typedef struct Book {
     char* name;//书名
     char* writer;//作者名
     char f_letter;//拼音首字母
-    int type;//书的种类
+    int type;//书的种类，0 科普类 1 教育类 2 小说类 3 传记类 4 历史类 5 诗歌类
     long long Isbn;//书的ISBN码
     long long p_date;//出版日期
+    struct Book* next;
 }Book;
 
 typedef struct User {
     char* name;//读者名字
-    int type;//读者类型
+    int type;//读者类型 0 本科生 1 研究生 2 博士生
     char f_letter;//读者名字拼音首字母
     long long password;//读者的密码
     char* account;//读者的账号
+    struct User* next;
 }User;
 
 typedef struct Administrator {
@@ -20,4 +22,5 @@ typedef struct Administrator {
     char f_letter;//管理员名字拼音首字母
     long long password;//管理员密码
     char* account;//管理员账号
+    struct Administrator* next;
 }Admin;
